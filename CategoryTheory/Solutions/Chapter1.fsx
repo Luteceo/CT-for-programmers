457eb7e71e3be8542a3a1becae1adb703ef4437c
Tomasz Jaskula <thomasv1000@hotmail.fr>
2018-10-09 18:46:07 +0200

    Implement identity function for a challenge

---
+++ CategoryTheory/Solutions/Chapter1.fsx
@@ -0,0 +1,11 @@
+(**
+    Implement, as best as you can, the identity function in your fa- vorite language (or the second favorite,
+    if your favorite language happens to be Haskell).
+**)
+
+let id a = a
+
+(**
+    Implementthecompositionfunctioninyourfavoritelanguage.
+    It takes two functions as arguments and returns a function that is their composition.
+**)
